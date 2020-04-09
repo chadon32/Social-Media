ba344060f72dffa2e4df95295e00b9161a9d620f
unknown <you@example.com>
2020-04-08 21:09:09 -0400

    Reorganizing 1:48

---
+++ functions/util/config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  
+    apiKey: "AIzaSyBmm3hi0ba-k2Y8BY6E2FsHODNP7C1cjXQ",
+    authDomain: "socialape-a2995.firebaseapp.com",
+    databaseURL: "https://socialape-a2995.firebaseio.com",
+    projectId: "socialape-a2995",
+    storageBucket: "socialape-a2995.appspot.com",
+    messagingSenderId: "1078486337507",
+    appId: "1:1078486337507:web:315bd53b5232703dd3c2d6",
+    measurementId: "G-VGP6YG9S0V"
+}
